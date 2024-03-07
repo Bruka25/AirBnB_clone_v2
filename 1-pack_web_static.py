120cbcd798828fd26389e7a48dfff2a24fa8bda3
Bruka25 <bkteshome862@gmail.com>
2024-03-07 18:22:46 +0300

    fabfile fix for compressing the archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -20,6 +20,6 @@ def do_pack():
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    if local("tar -cvzf {} web_static".format(tar_file)).failed is True:
         return None
     return tar_file

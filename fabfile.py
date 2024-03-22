653edda3dc31e3dd895798658e695a06478c659b
Bruka25 <bkteshome862@gmail.com>
2024-03-22 22:01:23 +0300

    python script that starts a web application

--- fabfile.py
+++ fabfile.py
@@ -6,21 +6,20 @@
 import os.path
 from datetime import datetime
 from fabric.api import local
-from paramiko.auth_handler import AuthHandler
 
 
 def do_pack():
     """Function for creating compressed zipped file"""
     date = datetime.utcnow()
     tar_file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year,
-                                                             date.month,
-                                                             date.day,
-                                                             date.hour,
-                                                             date.minute,
-                                                             date.second)
+                                                         date.month,
+                                                         date.day,
+                                                         date.hour,
+                                                         date.minute,
+                                                         date.second)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
-    if local("tar -cvzf {} web_static".format(tar_file)).failed is True:
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
     return tar_file
